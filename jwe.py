f7a044fe11030d51b8c325393d57f7c0061aa907
Chris Seto <chriskseto@gmail.com>
2015-10-11 16:57:44 -0400

    Initial Commit

---
+++ jwe.py
@@ -0,0 +1,62 @@
+import os
+import json
+import base64
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+backend = default_backend()
+
+
+def kdf(key, salt=b'You should really salt this yourself', length=32, iterations=10000):
+    return PBKDF2HMAC(
+        salt=salt,
+        length=length,
+        backend=backend,
+        iterations=iterations,
+        algorithm=hashes.SHA256(),
+    ).derive(key)
+
+
+def encrypt(data, key):
+    segments = []
+
+    header = {'alg': 'dir', 'enc': 'A256GCM'}
+    segments.append(base64.b64encode(json.dumps(header).encode()))
+
+    segments.append(b'')  # Keywrapping is for suckers
+
+    iv = os.urandom(16)
+    segments.append(base64.b64encode(iv))
+
+    encryptor = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend()).encryptor()
+    encryptor.authenticate_additional_data(segments[0])
+    ciphertext = encryptor.update(data) + encryptor.finalize()
+    segments.append(base64.b64encode(ciphertext))
+
+    segments.append(base64.b64encode(encryptor.tag))
+
+    return b'.'.join(segments)
+
+
+def decrypt(data, key):
+    # Generate a random 96-bit IV.
+    spl = data.split(b'.')
+
+    try:
+        header, _, iv, ciphertext, tag = [base64.b64decode(x) for x in spl]
+    except ValueError:
+        pass
+
+    encryptor = Cipher(
+        algorithms.AES(key),
+        modes.GCM(iv, tag),
+        backend=default_backend()
+    ).decryptor()
+
+    encryptor.authenticate_additional_data(spl[0])
+
+    return encryptor.update(ciphertext) + encryptor.finalize()
---
+++ setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+
+def parse_requirements(requirements):
+    with open(requirements) as f:
+        return [l.strip('\n') for l in f if l.strip('\n') and not l.startswith('#')]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    with open(os.path.join(package, '__init__.py'), 'rb') as init_py:
+        src = init_py.read().decode('utf-8')
+        return re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
+
+
+version = get_version('jwt')
+requirements = parse_requirements('requirements.txt')
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    long_description = readme.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    print('You probably want to also tag the version now:')
+    print(" git tag -a {0} -m 'version {0}'".format(version))
+    print(' git push --tags')
+    sys.exit()
+
+tests_require = [
+    'pytest==2.7.3',
+    'pytest-cov',
+    'pytest-runner',
+]
+
+needs_pytest = set(('pytest', 'test', 'ptr')).intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+setup(
+    name='PyJWE',
+    version=version,
+    description='JSON Web Encryption implementation in Python',
+    license='MIT',
+    keywords='jwe json encryption token security signing',
+    url='http://github.com/chrisseto/pyjwe',
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    long_description=long_description,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities',
+    ],
+    test_suite='tests',
+    setup_requires=pytest_runner,
+    tests_require=tests_require,
+    install_requires=requirements,
+    extras_require=dict(
+        test=tests_require,
+        crypto=['cryptography'],
+        flake8=[
+            'flake8',
+            'flake8-import-order',
+            'pep8-naming'
+        ]
+    ),
+)
